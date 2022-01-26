76503752c6535b0db6975fa80120302559df304e
George Wesley Lyon <georgewesley17@gmail.com>
2022-01-25 21:49:34 -0500

    Implementing Entry Displays
    Started implementation of entry displays

--- Assets/Scripts/DisplayEntries.cs
+++ Assets/Scripts/DisplayEntries.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DisplayEntries : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class DisplayEntries : MonoBehaviour
 
     [SerializeField] PerformanceTracker tracker;
     [SerializeField] Button button;
+    [SerializeField] TextMeshProUGUI text;
+    Vector3 entryDisplayTransform;
     void Start()
     {
         entries = tracker.performanceEntries;
@@ -18,13 +21,30 @@ public class DisplayEntries : MonoBehaviour
     }
     void Display(){
         GameObject EntryDisplayArea = GetComponentInChildren<VerticalLayoutGroup>().gameObject;
-        Vector3 transform = EntryDisplayArea.GetComponent<Transform>().position;
+        entryDisplayTransform = EntryDisplayArea.GetComponent<Transform>().position;
         foreach(PerformanceEntry entry in entries) {
-            //do stuff with entries
+            Button newButton = Instantiate<Button>(button, new Vector3(entryDisplayTransform.x, entryDisplayTransform.y, entryDisplayTransform.z), Quaternion.identity);
+            newButton.onClick.AddListener(() => OnPress(entry));
+            SetButtonText(newButton, entry);
+            newButton.transform.SetParent(EntryDisplayArea.transform);
         }
 
     }
-    void OnPress(PerformanceTracker performanceTracker) {
-        Debug.Log("Pressed " + performanceTracker.firstName);
+    void OnPress(PerformanceEntry performanceEntry) {
+        Debug.Log("Pressed " + performanceEntry.textDescription);
+    }
+
+    void SetButtonText(Button newButton, PerformanceEntry entry) {
+        TextMeshProUGUI newText = Instantiate<TextMeshProUGUI>(text, new Vector3(entryDisplayTransform.x, entryDisplayTransform.y, entryDisplayTransform.z), Quaternion.identity);
+        newText.text = "Category: " + entry.category;
+        newText.transform.SetParent(newButton.transform);
+
+        TextMeshProUGUI newText1 = Instantiate<TextMeshProUGUI>(text, new Vector3(entryDisplayTransform.x, entryDisplayTransform.y, entryDisplayTransform.z), Quaternion.identity);
+        newText1.text = "Subcategory: " + entry.subcategory;
+        newText1.transform.SetParent(newButton.transform);
+
+        TextMeshProUGUI newText2 = Instantiate<TextMeshProUGUI>(text, new Vector3(entryDisplayTransform.x, entryDisplayTransform.y, entryDisplayTransform.z), Quaternion.identity);
+        newText2.text = "Leader Name: " + entry.leaderName;
+        newText2.transform.SetParent(newButton.transform);
     }
 }
